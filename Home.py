625f50855506933d7f962587903a85417a157fee
jliao8112-boop <jliao8112@gmail.com>
2026-03-14 00:15:20 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,9 +4,10 @@ st.set_page_config(page_title="贏家思維與風控", page_icon="🏰", layout=
 
 st.markdown("""
     <div style='text-align: center; padding: 50px;'>
-        <h1 style='font-size: 3rem;'>🏰 量化旗艦指揮中心</h1>
+        <h1 style='font-size: 3rem;'>🏰 贏家思維與風控</h1>
         <p style='font-size: 1.2rem; color: #64748B;'>系統連線正常。請點擊左上角 <strong>「>」選單</strong> 切換戰略模組。</p>
     </div>
 
 """, unsafe_allow_html=True)
+
 
